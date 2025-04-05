b2d7cc9bc108f4801cfb8d135d30b95fac1fb597
iamtayyab007 <tayyabm336@gmail.com>
2025-04-05 11:33:10 +0500

    all files

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,26 +1,72 @@
-"use client";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useState } from "react";
+import { useDebounceValue } from "usehooks-ts";
+import { Toaster as Sonner } from "sonner";
+import { useRouter } from "next/navigation";
+import { signUpSchema } from "@/schemas/signUpSchema";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
 
-import { useSession, signIn, signOut } from "next-auth/react";
+export default function page() {
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const debounceUsername = useDebounceValue(username, 500);
+  const router = useRouter();
 
-export default function Component() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button
-        className="bg-orange-500 px-3 py-1 m-4 rounded"
-        onClick={() => signIn()}
-      >
-        Sign in
-      </button>
-    </>
-  );
+  //zod implementation
+  const form = useForm({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+  useEffect(() => {
+    const checkUsername = async () => {
+      if (debounceUsername) {
+        setIsCheckingUsername(true);
+        setUsernameMessage("");
+
+        try {
+          const response = await axios.get(
+            `/api/check-username-unique?username=${debounceUsername}`
+          );
+          setUsernameMessage(response.data.message);
+        } catch (error) {
+          const axiosError = error as AxiosError<ApiResponse>;
+          setUsernameMessage(
+            axiosError.response?.data.message ?? "Error checking username"
+          );
+        } finally {
+          setIsCheckingUsername(false);
+        }
+      }
+    };
+    checkUsername();
+  }, [debounceUsername]);
+
+  return <div>page</div>;
+}
+function zodResolver(
+  signUpSchema: z.ZodObject<
+    { username: z.ZodString; email: z.ZodString; password: z.ZodString },
+    "strip",
+    z.ZodTypeAny,
+    { username: string; email: string; password: string },
+    { username: string; email: string; password: string }
+  >
+):
+  | import("react-hook-form").Resolver<
+      import("react-hook-form").FieldValues,
+      any,
+      import("react-hook-form").FieldValues
+    >
+  | undefined {
+  throw new Error("Function not implemented.");
 }
